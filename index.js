1080e6baf035b96b43b38bfef29d0f5dd9515a11
Dolan Miu <dolan_miu@hotmail.com>
2016-10-28 23:07:01 +0100

    initial slack calling

---
+++ index.js
@@ -0,0 +1,37 @@
+const SlackWebhook = require('slack-webhook');
+const messages = require('./messages.json');
+
+const slack_url = process.env.npm_config_slackUrl;
+const errorType = process.env.npm_config_error;
+const humour = process.env.npm_config_humour;
+const isSuccessful = errorType == undefined;
+
+if (!slack_url) {
+    process.exit(9);
+}
+
+let colour;
+switch (errorType) {
+    case "test":
+        colour = "warning";
+        break;
+    case "build":
+        colour = "danger";
+        break;
+    case undefined:
+        colour = "good";
+        break;
+    default:
+        colour = "#439FE0"
+}
+
+const slack = new SlackWebhook(slack_url);
+
+slack.send({
+    text: 'some text',
+    attachments: [
+        // optional attachment data 
+    ],
+    username: 'new username',
+    icon_emoji: ':scream_cat:'
+});
